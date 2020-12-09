ff855ec0124b3b5346aea34bd5bc8983319fae48
mahdizaabi <mahdi.zaabi@gmail.com>
2020-12-09 14:17:04 +0100

    add

---
+++ 0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Unlock check"""
+    allKeys = []
+
+    for box in boxes:
+        for key in box:
+            allKeys.append(key)
+
+    for key in range(len(boxes)):
+        if key in boxes[key]:
+            if allKeys.count(key) == 1:
+                return False
+    return True
