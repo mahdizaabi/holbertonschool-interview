b8d9c1b47ffc82c4ca1c288ef346565bb1a22778
mahdi <mahdi.zaabi@gmail.com>
2021-01-21 19:48:09 +0000

    add

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * is_palindrome - check is a singly list is a palindrome .
+ * @head: head of the list .
+ * Return: 1 ? palindrome : 0.
+ */
+int is_palindrome(listint_t **head)
+{
+
+
+	listint_t *counter = NULL;
+	int count = 0;
+	int stack[512];
+	int endStack = 0;
+	int frontStack = 0;
+	int r = 0;
+
+	if (*head == NULL)
+		return (1);
+
+	counter = *head;
+
+	while (counter)
+	{
+		count = count + 1;
+		stack[endStack] = counter->n;
+		endStack++;
+		counter = counter->next;
+	}
+	endStack--;
+	r = endStack / 2 + 1;
+	while (endStack >= r)
+	{
+		if (stack[frontStack] != stack[endStack])
+			return (0);
+		printf("%d\n", stack[endStack]);
+		frontStack++;
+		endStack--;
+	}
+	return (1);
+
+
+
+
+}
