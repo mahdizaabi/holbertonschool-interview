fcb6d99075704448138797505956ac116bbb4a4a
mahdi <mahdi.zaabi@gmail.com>
2021-01-28 17:38:31 +0000

    add

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" log parsing """
+
+
+import sys
+
+def print(data, status):
+    """ print the log """
+    try:
+        print("File size: {}".format(data))
+        for key, value in sorted(status.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
+    except Exception as e:
+        pass
+
+status = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0}
+counter = 0
+data = 0
+try:
+    for line in sys.stdin:
+        if counter == 10:
+            print(data, status)
+            counter = 1
+
+        else:
+            counter = counter + 1
+        parsed = line.split()
+        try:
+            data = data + int(parsed[-1])
+        except Exception as e:
+            pass
+        try:
+            for key, value in status.items():
+                if key == parsed[-2]:
+                    status[key] = status[key] + 1
+        except Exception as e:
+            pass
+except KeyboardInterrupt:
+    print(data, status)
+    raise
