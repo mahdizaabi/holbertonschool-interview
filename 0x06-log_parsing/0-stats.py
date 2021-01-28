928ff8bd3738066fcc4fd8250dd98ba1593485ab
mahdi <mahdi.zaabi@gmail.com>
2021-01-28 15:28:20 +0000

    add

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,37 +1,48 @@
 #!/usr/bin/python3
 '''log parser algorithm'''
 
-
 import sys
+import re
 
 
-mydict = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0}
-file_size = 0
+data_Buffer = []
+somme = 0
+status = {
+    "200": [],
+    "301": [],
+    "400": [],
+    "401": [],
+    "403": [],
+    "404": [],
+    "405": [],
+    "500": []}
+counter = 0
+regex = '([(\d\.)]+) - \[(.*?)\] "(.*?)" (\d+) (\d+)'
+
 try:
-    for n, line in enumerate(sys.stdin, 1):
-        _line = line.split()
-        if len(_line) > 2:
-            status_code = _line[len(_line) - 2]
-            file_size += int(_line[len(_line) - 1])
-            if status_code in mydict:
-                mydict[str(status_code)] += 1
-        if n % 10 == 0:
-            print("File size: {}".format(file_size))
-            for k in sorted(mydict):
-                if mydict[k] > 0:
-                    print("{}: {}".format(k, mydict[k]))
+    for line in sys.stdin:
+        try:
+            if re.match(regex, line).groups()[3] in status.keys():
+                status[re.match(regex, line).groups()[3]].append(1)
+                data_Buffer.append(re.match(regex, line).groups()[4])
+                counter = counter + 1
+            else:
+                counter = counter + 1
+        except Exception as e:
+            counter = counter + 1
+        if counter == 10:
+            for i in data_Buffer:
+                somme = somme + int(i)
+            print('File size: {}'.format(somme))
+
+            for key, value in status.items():
+                if len(value) != 0:
+                    print('{}: {}'.format(key, len(value)))
+            counter = 0
 except KeyboardInterrupt:
     pass
 finally:
-    print('File size: {}'.format(file_size))
-    for k in sorted(mydict):
-        if mydict[k] > 0:
-            print("{}: {}".format(k, mydict[k]))
+    print('File size: {}'.format(somme))
+    for key, value in status.items():
+        if len(value) != 0:
+            print('{}: {}'.format(key, len(value)))
