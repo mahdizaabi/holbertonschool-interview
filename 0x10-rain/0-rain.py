a5c14c3cb98087fb88bf308a8c871c521e7f96c0
mahdi <mahdi.zaabi@gmail.com>
2021-04-15 02:59:11 +0000

    add

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" 0x10-rain """
+
+
+def getWall(walls, startingIndex):
+    """[function that calculate how much water
+    will be retained after it rains]
+
+    Args:
+        walls ([type]): [description]
+        startingIndex ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    for value in walls[startingIndex+1:]:
+        if value != 0:
+            return value
+
+
+def pop_zeros(items):
+    """[function that calculate how much water
+    will be retained after it rains]
+
+    Args:
+        items ([type]): [description]
+    """
+    while items[-1] == 0:
+        items.pop()
+
+
+def BorderLine(berg, index):
+    """[function that calculate how much water
+    will be retained after it rains]
+    """
+    if berg[index+1] == 0:
+        return True
+
+
+def rain(walls):
+    """[function that calculate how much water
+    will be retained after it rains]
+
+    Args:
+        walls ([type]): [description]
+    """
+
+    if len(walls) == 0:
+        return 0
+    total = 0
+    pop_zeros(walls)
+    for index, value in enumerate(walls[:-1]):
+        if value != 0 and BorderLine(walls, index):
+            nextWallValue = getWall(walls, index)
+            if value <= nextWallValue:
+                total = total + value
+            else:
+                total = total + nextWallValue
+    return total
