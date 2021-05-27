c50e126a1e6a7c9b1ba5abc691a1b56a384ab518
mahdi <mahdi.zaabi@gmail.com>
2021-05-27 14:30:48 +0000

    add

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+"""
+0x16 - Rotate 2D Matrix
+"""
+
+
 def rotate_2d_matrix(matrix):
-    """ Rotate 90° matrix"""
+    """ Rotate 90° matrix Rotate 2D Matrix"""
     temp = matrix.copy()
     matrix.clear()
     for y in range(len(temp[0])):
