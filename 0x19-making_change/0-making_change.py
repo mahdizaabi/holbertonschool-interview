d11ff28a1fbec70f60ce307cde9c3e5c192e2d3c
crxs14 <59511107+mahdizaabi@users.noreply.github.com>
2021-06-24 23:57:10 +0200

    Update 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -14,14 +14,14 @@ def makeChange(coins, total):
     Returns:
         [type]: [description]
     """
-coinsNumber = 0
+ coinsCombination = 0
     if total <= 0:
         return 0
     coins.sort()
     coins.reverse()
-    for coin in coins:
-        coinsNumber += total // coin
-        total = total % coin
+    for c in coins:
+        coinsCombination += total // c
+        total = total % c
     if (total != 0):
         return -1
-    return coinsNumber
+    return coinsCombination
