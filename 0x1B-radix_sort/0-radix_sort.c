28b483fdbf17ffe41f1d38afe2817d961392c25e
mahdi <mahdi.zaabi@gmail.com>
2021-07-08 22:36:46 +0000

    add

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,48 +1,14 @@
-#include <stdlib.h>
 #include "sort.h"
 
-
 /**
- * get_digit - Returns the numbers of digits of a number.
- * @number:integer
- * @n: interger
- * Return: integer, number of digits
- */
-int get_digit(int number, int n)
-{
-if (n == 0)
-{
-return (number % 10);
-}
-int r = 10;
-for (int x = 0; x < n - 1; x++)
-{
-r = r *r;
-}
-return (number / r % 10);
-}
-/**
- * getLength - Returns the numbers of digits of a number.
- * @number: interger
- * Return: integer, number of digits
- */
-int getLength(int number)
-{
-int count = 0;
-while (number != 0)
-{
-number /= 10;
-++count;
-}
-return (count);
-}
-/**
- * get_max - Returns the number of digits of the biggest int in the array.
- * @array: array to seach into.
+ * getMaximum - find the maximum digit of an array
+ *
+ * @array: array of integers
  * @size: size of the array.
+ *
  * Return: the biggest element of the array.
  */
-int get_max(int *array, int size)
+int getMaximum(int *array, int size)
 {
 int maxNum, i;
 
@@ -56,49 +22,69 @@ maxNum = array[i];
 return (maxNum);
 }
 /**
- * radix_sort - Sorts an array of ints in ascending order using the radix sort.
- * @array: array to sort
+ * getDigit - get the significant digit
+ * @number: num
+ * @position: postion of the digit to be extracted
+ * Return: int
+ */
+int getDigit(int number, int position)
+{
+	return ((number / position) % 10);
+}
+
+/**
+ * countingSort - the main func
+ * @array: array
+ * @exp: exp
  * @size: size of the array
- **/
+ * Return: None
+ */
+void countingSort(int *array, int exp, int size)
+{
+	int i;
+	int digit;
+	int counting[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int *temp = NULL;
+	int k;
+
+
+	temp = (int *)malloc(sizeof(int) * size);
+	if (!temp)
+		return;
+
+	for (i = 0; i < size; i++)
+		counting[getDigit(array[i], exp)]++;
+
+	for (i = 1; i < 10; i++)
+		counting[i] += counting[i - 1];
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		digit = getDigit(array[i], exp);
+		k = --counting[digit];
+		temp[k] = array[i];
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = temp[i];
+
+	print_array(array, (size_t)size);
+	free(temp);
+}
+/**
+ * radix_sort - sort an array using the radix algo
+ * @array: array to sort
+ * @size: size of tharray
+ * Return: nothing void
+ */
 void radix_sort(int *array, size_t size)
 {
+	int maximum = getMaximum(array, (int)size);
+	int exp = 1;
 
-if (array == NULL || size < 2)
-return;
+	if (array == NULL || size <= 2)
+		return;
 
-int *sortedArray = NULL;
-int k = 0;
-int flag = 0;
-sortedArray = malloc(sizeof(int) * size);
-if (sortedArray == NULL)
-return;
-int max = get_max(array, size);
-int length = getLength(max) + 1;
-for (int pos = 0; pos < length; pos++)
-{
-k = 0;
-if (flag != 0)
-{
-for (int j = 0; j < (int)size; j++)
-{
-array[j] = sortedArray[j];
-}
-print_array(array, size);
-
-}
-for (int rindex = 0; rindex < 10; rindex++)
-{
-for (int aindex = 0; aindex < (int)size; aindex++)
-{
-if (get_digit(array[aindex], pos) == rindex)
-{
-sortedArray[k] = array[aindex];
-k++;
-}
-flag = 1;
-}
-}
-}
-free(sortedArray);
-sortedArray = NULL;
+	for (; maximum / exp > 0; exp *= 10)
+		countingSort(array, exp, (int)size);
 }
