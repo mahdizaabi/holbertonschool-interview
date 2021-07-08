57eaf3ed4e87838137c6886d8d15acd02ba287a7
mahdi <mahdi.zaabi@gmail.com>
2021-07-08 22:45:09 +0000

    add

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -82,7 +82,7 @@ void radix_sort(int *array, size_t size)
 	int maximum = getMaximum(array, (int)size);
 	int exp = 1;
 
-	if (size < 2)
+	if (size < 2 || !array)
 		return;
 
 	for (; maximum / exp > 0; exp *= 10)
