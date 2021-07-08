8c19c4e86ea6c6334f1a0dfeabd63ad830bdc337
mahdi <mahdi.zaabi@gmail.com>
2021-07-08 19:57:54 +0000

    add

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -1,9 +1,11 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef SORT_H
+#define SORT_H
 
+#include <stddef.h>
+#include <stdlib.h>
 void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
 int get_max(int *array, int size);
 int get_digit(int number, int n);
 int getLength(int number);
-#endif
+#endif /* SORT_H */
