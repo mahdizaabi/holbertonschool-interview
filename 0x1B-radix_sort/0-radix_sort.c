5ac38209f64ce2b0c76da7e5b60381be0d14ef46
mahdi <mahdi.zaabi@gmail.com>
2021-07-08 20:09:10 +0000

    add

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,7 +1,41 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * get_digit - Returns the numbers of digits of a number.
+ * @number:integer
+ * @n: interger
+ * Return: integer, number of digits
+ */
+int get_digit(int number, int n)
+{
+if (n == 0)
+{
+return (number % 10);
+}
+int r = 10;
+for (int x = 0; x < n - 1; x++)
+{
+r = r *r;
+}
+return (number / r % 10);
+}
+/**
+ * getLength - Returns the numbers of digits of a number.
+ * @number: interger
+ * Return: integer, number of digits
+ */
+
+int getLength(int number)
+{
+int count = 0;
+while (number != 0)
+{
+number /= 10;
+++count;
+}
+return (count);
+}
 /**
  * get_max - Returns the number of digits of the biggest int in the array.
  *
@@ -12,55 +46,17 @@
  */
 int get_max(int *array, int size)
 {
-	int maxNum, i;
+int maxNum, i;
 
-	maxNum = array[0];
+maxNum = array[0];
 
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > maxNum)
-			maxNum = array[i];
-	}
-
-	return (maxNum);
-}
-
-/**
- * count_sort - Sort an array of ints according to their Ith digit.
- *
- * @array: the array to sort.
- * @size: the size of the array.
- * @di: the index of the digit according to which the array will be sorted.
- */
-void count_sort(int *array, int size, int di)
+for (i = 1; i < size; i++)
 {
-	int ce, cd, ce2, cd2, tmp, x;
-
-	ce = 0;
-	while (ce < size)
-	{
-		cd = (array[ce] / di) % 10;
-		ce2 = ce;
-		while (ce2 < size)
-		{
-			cd2 = (array[ce2] / di) % 10;
-			if (cd > cd2)
-			{
-				tmp = array[ce2];
-				for (x = ce2 - 1; x >= ce; x--)
-				{
-					array[x + 1] = array[x];
-				}
-				array[ce] = tmp;
-				ce = -1;
-				break;
-			}
-			ce2++;
-		}
-		ce++;
-	}
+if (array[i] > maxNum)
+maxNum = array[i];
 }
-
+return (maxNum);
+}
 /**
  * radix_sort - Sorts an array of ints in ascending order using the radix sort.
  *
@@ -69,17 +65,41 @@ void count_sort(int *array, int size, int di)
  */
 void radix_sort(int *array, size_t size)
 {
-	int s, max, i;
 
-	if (array == NULL || size < 2)
-		return;
-	s = (int) size;
-	max = get_max(array, s);
-	i = 1;
-	while ((max / i) > 0)
-	{
-		count_sort(array, s, i);
-		print_array(array, size);
-		i *= 10;
-	}
+if (array == NULL || size < 2)
+return;
+
+
+int *sortedArray = NULL;
+int k = 0;
+int flag = 0;
+
+for (int pos = 0; pos < getLength(get_max(array, size)) + 1; pos++)
+{
+k = 0;
+if (flag != 0)
+{
+for (int j = 0; j < (int)size; j++)
+{
+array[j] = sortedArray[j];
+}
+print_array(array, size);
+free(sortedArray);
+sortedArray = NULL;
+}
+sortedArray = malloc(sizeof(int) * size);
+
+for (int rindex = 0; rindex < 10; rindex++)
+{
+for (int aindex = 0; aindex < (int)size; aindex++)
+{
+if (get_digit(array[aindex], pos) == rindex)
+{
+sortedArray[k] = array[aindex];
+k++;
+}
+flag = 1;
+}
+}
+}
 }
