a2886f9e7b78b7ec2dcef5dae77aac8d5848ba14
mahdi <mahdi.zaabi@gmail.com>
2021-07-29 19:56:47 +0000

    add

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - Wild Compare
+ * @s1: string to match
+ * @s2: string to compare
+ *
+ * Return: 1 if the string is found, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (!*s2);
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
